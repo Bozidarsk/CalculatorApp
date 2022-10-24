68c9d3aa0eb8f17ec17127836c4836fc497bbffc
Bozidarsk <bozidarkabahcijski@gmail.com>
2022-10-24 16:28:58 +0300

    Created repository.

---
+++ CalculatorApp/CalculatorApp/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculatorApp.Pages;
+using Xamarin.Forms;
+
+namespace CalculatorApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(BasicCalculatorPage), typeof(BasicCalculatorPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//BasicCalculatorPage");
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Pages/BasicCalculatorPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorApp.Pages
+{
+    public partial class BasicCalculatorPage : ContentPage
+    {
+        public BasicCalculatorPage() { InitializeComponent(); }
+
+        private string input = "0";
+        private bool use2nd = false;
+
+        private void UpdateDisplay(string content) { display.Text = content; }
+        public void OnButtonPress(object sender, EventArgs e) 
+        {
+            string arg = ((Button)sender).ClassId;
+            switch (arg) 
+            {
+                case "=":
+                    input = CalculatorApp.BasicCalculator.Solve(input);
+                    break;
+                case "cls":
+                    input = "0";
+                    break;
+                case "bs":
+                    if (input.Length <= 1) { input = "0"; break; }
+                    input = input.Remove(input.Length - 1, 1);
+                    if (input == "-") { input = "0"; }
+                    break;
+                case "2nd":
+                    use2nd = !use2nd;
+                    SwitchButtons("BasicCalculatorPage.xaml");
+                    break;
+                default:
+                    if (input == "0") { input = ""; }
+                    input += arg;
+                    break;
+            }
+
+            UpdateDisplay(input);
+        }
+
+        public void SwitchButtons(string uri) 
+        {
+            pi.Text = (use2nd) ? "e" : "π";
+            pi.ClassId = (use2nd) ? "e" : "pi";
+
+            log.Text = (use2nd) ? "10^x" : "log";
+            log.ClassId = (use2nd) ? "10^(" : "log(";
+
+            sin.Text = (use2nd) ? "asin" : "sin";
+            sin.ClassId = (use2nd) ? "asin(" : "sin(";
+
+            cos.Text = (use2nd) ? "acos" : "cos";
+            cos.ClassId = (use2nd) ? "acos(" : "cos(";
+
+            tan.Text = (use2nd) ? "atan" : "tan";
+            tan.ClassId = (use2nd) ? "atan(" : "tan(";
+
+            carret.Text = (use2nd) ? "!" : "^";
+            carret.ClassId = (use2nd) ? "!" : "^";
+
+            sqrt.Text = (use2nd) ? "1/x" : "√";
+            sqrt.ClassId = (use2nd) ? "^(-1)" : "√";
+        }
+    }
+}
